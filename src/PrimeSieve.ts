a269ccfdaa1eb58dedb189ce1e05466229ee742f
Gordon McAllister <gordonmc8@gmail.com>
2016-10-24 11:51:00 +0100

    Implementation of Sieve of Eratosthenes

--- src/PrimeSieve.ts
+++ src/PrimeSieve.ts
@@ -1,7 +1,38 @@
 export default class PrimeSieve {
 
     calculatePrimes(sieveSize: number) : number[] {
-        return [];
+        
+        // Initialise the sieve
+        let sieve: boolean[] = [];
+        for (let i = 0; i < sieveSize; i++) {
+            sieve[i] = true;
+        }
+
+        // Sieve with primes up to the root of the sieve size
+        let primes: number[] = [];
+        let candidate = 2;
+        let maxCandidate = Math.floor(Math.sqrt(sieveSize));
+        for (; candidate <= maxCandidate; candidate++) {
+            // If the sieve is false, this is a composite number
+            if (!sieve[candidate-1]) continue;
+
+            // This is prime, store it and calculate its composites for the rest of the sieve
+            primes.push(candidate);
+            let multiple = candidate * candidate;
+            while (multiple <= sieveSize) {
+                sieve[multiple-1] = false;
+                multiple += candidate;
+            }
+        }
+
+        // Collect the remaining primes from the sieve
+        for (let i = maxCandidate + 1; i < sieveSize; i++) {
+            if (sieve[i-1]) {
+                primes.push(i);
+            }
+        }
+
+        return primes;
     }
 
 }
