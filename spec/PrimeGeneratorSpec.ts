53c22d27fb252aba31123e4e278d8db970f32b1a
Gordon McAllister <gordonmc8@gmail.com>
2016-10-24 12:01:59 +0100

    PrimeGenerator now uses the PrimeSieve to generate the list of primes it supplies.

--- spec/PrimeGeneratorSpec.ts
+++ spec/PrimeGeneratorSpec.ts
@@ -4,12 +4,12 @@ import PrimeGenerator from '../src/PrimeGenerator';
 describe("Prime Generator tests", function () {
 
     it("Should return 2 as first prime", function () {
-        let generator = new PrimeGenerator();
+        let generator = new PrimeGenerator(100);
         expect(generator.next()).toEqual(2);
     });
 
     it("Should return 2 and 3 as first two primes", function () {
-        let generator = new PrimeGenerator();
+        let generator = new PrimeGenerator(100);
         let primeList: number[] =  [];
         
         primeList.push(generator.next());
--- src/PrimeGenerator.ts
+++ src/PrimeGenerator.ts
@@ -1,19 +1,34 @@
+import PrimeSieve from './PrimeSieve';
+
+/**
+ * Simple generator that could be sufficient when there is a bound on the maximum prime number to be generated.
+ * 
+ */
 export default class PrimeGenerator {
 
     index: number;
+    primes: number[];
+    upperPrimeBound: number;
 
-    constructor() {
+    constructor(upperPrimeBound: number) {
         this.index = 0;
+        this.upperPrimeBound = upperPrimeBound;
     }
 
     next() : number {
-        ++this.index;
 
-        if (this.index === 1) {
-            return 2;
+        if (this.index == 0) {
+            var sieve = new PrimeSieve();
+            this.primes = sieve.calculatePrimes(this.upperPrimeBound);
         }
 
-        return null;
+        if (this.index >= this.primes.length) {
+            return null;
+        }
+
+        ++this.index;
+
+        return this.primes[this.index-1];
     }
 
 }
